78f90cd24f88d1dba3e91e50cc5332268a1d200b
Tom Vamvanij <tom.vamvanij@gmail.com>
2018-10-28 19:13:27 -0700

    Delete module w/ old name
    Will use rename next time.

--- api/make-quiz.js
+++
@@ -1,9 +0,0 @@
-import { success, failure } from './libs/response';
-
-export default function(event, context, callback) {
-  if (false) {
-    callback(null, success({ message: 'Hello, World' }));
-  } else {
-    callback(null, failure({ status: false, error: 'Item not found.' }));
-  }
-}
