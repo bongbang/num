63d365f9ef6b2de0267cf8e5f869d2c2bf8f2b3b
Tom Vamvanij <tom.vamvanij@gmail.com>
2018-10-31 15:54:00 -0700

    Add working quiz prototype

--- api/make_quiz.py
+++ api/make_quiz.py
@@ -1,8 +1,17 @@
 import json
+from modules import omnibus as modules
 
-def get_quiz(event, context):
+def make_quiz(event, context):
+    n = 5 # TODO parameterize
 
-    result = {'message': 'Making {} quiz...'.format(event['pathParameters']['id'])}
+    try:
+        module = getattr(modules, event['pathParameters']['id'])
+        message = [module() for i in range(n)]
+    except KeyError:
+        status_code = 404
+        message = 'Module doesn''t exist'
+
+    result = {'message': message}
 
     # create a response
     response = {
