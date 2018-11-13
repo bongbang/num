0f01cd2a617185d029b37dbf0d9685c5148a7eb9
Tom Vamvanij <tom.vamvanij@gmail.com>
2018-11-12 16:06:47 -0800

    Display quiz

--- client/src/containers/Quiz.js
+++ client/src/containers/Quiz.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import { API } from 'aws-amplify';
-import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { PageHeader, Well, FormGroup, FormControl } from 'react-bootstrap';
 // import { s3Upload } from '../libs/awsLib';
 // import config from '../config';
 
@@ -11,64 +11,47 @@ export default class Quiz extends Component {
 
     this.state = {
       isLoading: true,
-      questions: []
+      questionNumber: 1
     };
+
+    this.quiz = [];
   }
 
   async componentDidMount() {
     // console.log('id: ' + this.props.match.params.id);
     try {
-      const questions = await this.questions();
-      this.setState({ questions });
-      console.log(this.state.questions);
+      this.quiz = await API.get('num', `/module/${this.props.match.params.id}`);
     } catch (e) {
       alert(e);
     }
+
+    this.setState({ isLoading: false });
   }
 
-  questions() {
-    return API.get('num', `/module/${this.props.match.params.id}`);
-  }
-
-  renderNotesList(notes) {
-    return [{}].concat(notes).map(
-      (note, i) =>
-        i !== 0 ? (
-          <ListGroupItem
-            key={note.noteId}
-            href={`/notes/${note.noteId}`}
-            onClick={this.handleNoteClick}
-            header={note.content.trim().split('\n')[0]}
-          >
-            {'Created: ' + new Date(note.createdAt).toLocaleString()}
-          </ListGroupItem>
-        ) : (
-          <ListGroupItem
-            key="new"
-            href="/notes/new"
-            onClick={this.handleNoteClick}
-          >
-            <h4>
-              <b>{'\uFF0B'}</b> Create a new note
-            </h4>
-          </ListGroupItem>
-        )
-    );
-  }
-
-  renderNotes() {
-    return (
-      <div className="notes">
-        <PageHeader>Questions</PageHeader>
-        <p>Look in console for questions.</p>
-        <ListGroup>
-          {!this.state.isLoading && this.renderNotesList(this.state.questions)}
-        </ListGroup>
-      </div>
-    );
-  }
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.questionNumber < this.quiz.questions.length) {
+      this.setState({ questionNumber: this.state.questionNumber + 1 });
+    } else {
+      alert('End of quiz. Go to report.');
+    }
+  };
 
   render() {
-    return <div className="Home">{this.renderNotes()}</div>;
+    return (
+      <div className="Home">
+        <PageHeader>Quiz</PageHeader>
+        <p>{this.state.questionNumber}.</p>
+        <Well>
+          {!this.state.isLoading &&
+            this.quiz.questions[this.state.questionNumber - 1]}
+        </Well>
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="answer">
+            <FormControl />
+          </FormGroup>
+        </form>
+      </div>
+    );
   }
 }
