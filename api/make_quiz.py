fcd09b305fc4dc3fc9414655da6c8e5d89837cf1
Tom Vamvanij <tom.vamvanij@gmail.com>
2019-02-20 14:27:19 -0800

    Tighten error-handling & add no-module event

--- api/make_quiz.py
+++ api/make_quiz.py
@@ -4,21 +4,18 @@ import json
 def make_quiz(event, context):
     n = 5 # TODO parameterize
     requested_module = event['pathParameters']['id'] #TODO clean up names
+    status_code = 404 #TODO make individual codes for errors?
 
     try:
         quiz_module = import_module('quiz_modules.' + requested_module)
-        status_code = 200
         quiz_set = quiz_module.main(n)
-        try:
-            message = [{'question': item[0], 'answer': item[1]} for item in quiz_set]
-        except KeyError:
-            status_code = 404 # or something
-            message = quiz_set
+        message = [{'question': item[0], 'answer': item[1]} for item in quiz_set]
+        status_code = 200
     except ModuleNotFoundError:
-        status_code = 404
         message = 'Module "{}" doesn''t exist'.format(requested_module)
+    except KeyError:
+        message = quiz_set
     except Exception as e:
-        status_code = 404
         message = getattr(e, 'message', repr(e))
 
 
