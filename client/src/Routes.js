b73e1e353875d9e30116d8847c5338bb3d0fdf86
Tom Vamvanij <tom.vamvanij@gmail.com>
2019-02-22 16:19:11 -0800

    Remove obsolete mock report

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
 
 import Home from './containers/Home';
-import Report from './containers/Report';
+// import Report from './containers/Report';
 import Quiz from './routes/Quiz';
 import NotFound from './containers/NotFound';
 import Report2 from './routes/Report/mock';
@@ -12,8 +12,6 @@ import Report2 from './routes/Report/mock';
 export default ({ childProps }) => (
   <Switch>
     <Route path="/" exact component={Home} />
-
-    <Route path="/report" exact component={Report} />
 
     <AppliedRoute path="/quiz/:id" exact component={Quiz} props={childProps} />
     <Route path="/report2" exact component={Report2} />
